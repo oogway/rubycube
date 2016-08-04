3bc33b54ce3f9f5fc7fde1e3445154c2814b6226
aag <code.aa@gdbl.me>
2016-08-04 14:23:36 +0530

    commiting files missing from previous commit

---
+++ examples/traits.rb
@@ -0,0 +1,68 @@
+ENV['RUBY_CUBE_TYPECHECK'] = "1"
+require_relative '../lib/cube'
+EmailNotifierT = Cube.trait do
+  def notify
+    puts "email sent to #{email}"
+  end
+  requires_interface Cube.interface {
+    proto(:email) { String }
+  }
+end
+
+AndroidNotifierT = Cube.trait do
+  def notify
+    puts "push notification sent to android device #{mobile_number}"  
+  end
+
+  requires_interface Cube.interface {
+    proto(:mobile_number) { String }
+  }
+end
+
+IOSNotifierT = Cube.trait do
+  def notify
+    puts "push notification sent to ios device #{mobile_number}"  
+  end
+
+  requires_interface Cube.interface {
+    proto(:mobile_number) { String }
+  }
+end
+
+CombinedNotifierT = Cube.trait do
+  def notify
+    mobile_notify
+    email_notify
+  end
+  requires_interface Cube.interface {
+    proto(:mobile_notify)
+    proto(:email_notify)
+  }
+end
+
+AndroidCombinedNotifier = Cube.trait.with_trait(AndroidNotifierT, rename: { notify: :mobile_notify })
+                              .with_trait(EmailNotifierT, rename: { notify: :email_notify })
+                              .with_trait(CombinedNotifierT)
+
+IOSCombinedNotifier = Cube.trait.with_trait(IOSNotifierT, rename: { notify: :mobile_notify })
+                          .with_trait(EmailNotifierT, rename: { notify: :email_notify })
+                          .with_trait(CombinedNotifierT)
+
+MobileEmailUser = Cube.interface {
+  proto(:email) { String }
+  proto(:mobile_number) { String }
+}
+
+Services = {
+  android: AndroidCombinedNotifier.wrap(MobileEmailUser),
+  ios: IOSCombinedNotifier.wrap(MobileEmailUser)
+}
+
+User = Cube[Struct.new(:email, :mobile_number, :type)].as_interface(MobileEmailUser)
+
+u1 = User.new('u1@foo.com', '1234567890', :android)
+u2 = User.new('u2@foo.com', '1234567899', :ios)
+
+[u1, u2].each do |u|
+  Services[u.type].new(u).notify
+end
---
+++ lib/cube/toplevel.rb
@@ -0,0 +1,97 @@
+module Cube
+
+  def self.[](mod)
+    return mod if mod.is_a?(CubeMethods)
+    unless mod.is_a?(Class)
+      raise ArgumentError, "Only classes can be be converted to Cube classes"
+    end
+    Class.new(mod).extend(CubeMethods)
+  end
+
+  class << self
+    alias_method :from, :[]
+  end
+
+  def self.interface(&block)
+    mod = Module.new
+    mod.extend(Cube::Interface)
+    mod.instance_variable_set('@ids', {})
+    mod.instance_eval(&block)
+    mod
+  end
+
+  def self.trait(&blk)
+    m = Module.new.extend(Cube::Trait)
+    m.module_exec(&blk) if block_given?
+    m
+  end
+
+  def self.check_type_spec(t, v, &blk)
+    if t.is_a?(Set)
+      if v.is_a?(Set)
+        if v != t
+          raise Cube::Interface::TypeMismatchError, "#{t.to_a} is not eql to #{v.to_a}"
+        end
+        return true
+      end
+      unless t.any? { |tp| check_type(tp, v, &blk) rescue false }
+        raise Cube::Interface::TypeMismatchError,
+          "#{v.inspect} is not any of #{t.to_a}"
+      end
+      return
+    end
+    if t.is_a? Array
+      raise Cube::Interface::TypeMismatchError,
+        "#{v} is not an Array" unless v.is_a? Array
+      check_type(t.first, v.first, &blk)
+      check_type(t.first, v.last, &blk)
+      return
+    end
+    raise Cube::Interface::TypeMismatchError, "#{v.inspect} is not type #{t}" unless blk.call(t, v) 
+    true
+  end
+
+  if ENV['RUBY_CUBE_TYPECHECK'].to_i > 0
+    def self.check_type(t, v)
+      if t.is_a?(Set)
+        unless t.any? { |tp| check_type(tp, v) rescue false }
+          raise Cube::Interface::TypeMismatchError,
+                "#{v.inspect} is not any of #{t.to_a}"
+        end
+        return
+      end
+      if t.is_a? Array
+        raise Cube::Interface::TypeMismatchError,
+              "#{v} is not an Array" unless v.is_a? Array
+        check_type(t.first, v.first)
+        check_type(t.first, v.last)
+        return
+      end
+      raise Cube::Interface::TypeMismatchError, "#{v.inspect} is not type #{t}" unless v.is_a? t
+      true
+    end
+  else
+    def self.check_type(*_); end
+  end
+
+  module CubeMethods
+    def as_interface(iface, runtime_checks: true)
+      raise ArgumentError, "#{iface} is not a Cube::Interface" unless iface.is_a?(Cube::Interface)
+      implements = lambda { |this|
+        unless this.is_a? Class
+          raise "Non-Class modules should not implement interfaces"
+        end
+        this.instance_variable_set(:@__interface_runtime_check, true) if runtime_checks
+        this.include(iface)
+      }
+      implements.call(clone)
+    end
+
+    def shell_implements(mod)
+      instance_variable_set(:@__interface_runtime_check, false)
+      instance_variable_set(:@__interface_arity_skip, true)
+      include(mod)
+    end
+  end
+end
+
